3f5793d05caa326028e62b6c63934c5e5d0805e7
roken-08 <avngravngr@gmail.com>
2025-09-12 00:17:11 +0530

    Adding a sample code file

---
+++ code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> num;
+
+    if (isPrime(num)) {
+        std::cout << num << " is a prime number." << std::endl;
+    } else {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
