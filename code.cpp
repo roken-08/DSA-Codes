5c77055b8974e25e9036b40e8ca417c41901582b
roken-08 <avngravngr@gmail.com>
2025-09-12 00:19:23 +0530

    Added a sample text file

--- code.cpp
+++ code.cpp
@@ -2,16 +2,16 @@
 #include <cmath>
 
 bool isPrime(int n) {
-    if (n <= 1) {
+    if(n <= 1){
         return false;
     }
-    if (n == 2) {
+    if(n == 2){
         return true;
     }
-    if (n % 2 == 0) {
+    if(n % 2 == 0){
         return false;
     }
-    for (int i = 3; i <= sqrt(n); i += 2) {
+    for(int i = 3; i <= sqrt(n); i += 2){
         if (n % i == 0) {
             return false;
         }
@@ -24,9 +24,9 @@ int main() {
     std::cout << "Enter a positive integer: ";
     std::cin >> num;
 
-    if (isPrime(num)) {
+    if(isPrime(num)){
         std::cout << num << " is a prime number." << std::endl;
-    } else {
+    }else{
         std::cout << num << " is not a prime number." << std::endl;
     }
 
